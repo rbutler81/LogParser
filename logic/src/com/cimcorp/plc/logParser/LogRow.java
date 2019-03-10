28e19e7a8b3fa8977c57a4b464ede7019ebf6b09
RyanB <rbutler@gmail.com>
2019-03-11 07:53:38 +1000

    LogParser first Commit

---
+++ logic/src/com/cimcorp/plc/logParser/LogRow.java
@@ -0,0 +1,216 @@
+package com.cimcorp.plc.logParser;
+
+import csvUtils.CSVWriter;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.TimeZone;
+
+public class LogRow implements CSVWriter {
+
+    private Calendar dateTime;
+    private double excelDateTime;
+    private long id;
+    private long level;
+    private long colour;
+    private String header;
+    private String description;
+    private int crates;
+    private List<String> columnLabel;
+
+    public LogRow(){}
+
+    public LogRow(String[] str){
+        parseDateTime(str[0]);
+        this.id = Integer.parseInt(str[1]);
+        this.level = Integer.parseInt(str[2]);
+        this.colour = Integer.parseInt(str[3]);
+        this.header = str[4];
+        this.description = str[5];
+        if (Array.getLength(str) > 6) {
+            for (int i=6; i < Array.getLength(str); i++){
+                if (str[i].indexOf("Crates") != -1){
+                    int k = str[i].indexOf("Crates");
+                    int j = str[i].indexOf(" ",k);
+                    crates = Integer.parseInt(str[i].substring(j+1));
+                }
+                else {
+                    try {
+                        crates = Integer.parseInt(str[i]);
+                    } catch (NumberFormatException e) {
+
+                    }
+                }
+
+            }
+        }
+    }
+
+    public List<String> getColumnLabel() {
+        return columnLabel;
+    }
+
+    public LogRow setColumnLabel(List<String> columnLabel) {
+        this.columnLabel = columnLabel;
+        return this;
+    }
+
+    public Calendar getDateTime() {
+        return dateTime;
+    }
+
+    public LogRow setDateTime(Calendar dateTime) {
+        this.dateTime = dateTime;
+        return this;
+    }
+
+    public double getExcelDateTime() {
+        return excelDateTime;
+    }
+
+    public LogRow setExcelDateTime(double excelDateTime) {
+        this.excelDateTime = excelDateTime;
+        return this;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public LogRow setId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public long getLevel() {
+        return level;
+    }
+
+    public LogRow setLevel(long level) {
+        this.level = level;
+        return this;
+    }
+
+    public long getColour() {
+        return colour;
+    }
+
+    public LogRow setColour(long colour) {
+        this.colour = colour;
+        return this;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public LogRow setHeader(String header) {
+        this.header = header;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LogRow setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public int getCrates() {
+        return crates;
+    }
+
+    public LogRow setCrates(int crates) {
+        this.crates = crates;
+        return this;
+    }
+
+    private void parseDateTime(String s) {
+        Calendar c = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        c.clear();
+        c.setLenient(false);
+
+        Calendar excelRef = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        excelRef.clear();
+        excelRef.setLenient(false);
+        excelRef.set(1900,01,01);
+        long excelRefDays = (excelRef.getTimeInMillis() * -1) / 1000 / 60 / 60 / 24;
+
+        int i = s.indexOf("/");
+        int month = Integer.parseInt(s.substring(0,i));
+        int j = s.indexOf("/",i+1);
+        int day = Integer.parseInt(s.substring(i+1,j));
+        int year = Integer.parseInt(s.substring(j+1,j+5));
+
+        i = s.indexOf(" ");
+        j = s.indexOf(":");
+        int hour = Integer.parseInt(s.substring(i+1,j));
+        i = s.indexOf(":",j+1);
+        int minute = Integer.parseInt(s.substring(j+1,i));
+        j = s.indexOf(".",i);
+        int second = Integer.parseInt(s.substring(i+1,j));
+        int milliSecond = Integer.parseInt(s.substring(j+1,s.length())) / 1000;
+
+        c.set(year,month,day,hour,minute,second);
+        long k = c.getTimeInMillis() + (long)milliSecond;
+        c.setTimeInMillis(k);
+        this.setDateTime(c);
+
+        double excelTime = (long) k;
+        excelTime = (excelTime / 1000 / 60 / 60 / 24) + excelRefDays + 2;
+
+        double timeInMillis = ((hour*1000*60*60)+(minute*1000*60)+(second*1000)+milliSecond) / (1000*60*60*24);
+        excelTime = excelTime + timeInMillis;
+        this.setExcelDateTime(excelTime);
+    }
+
+    private String[] buildCsvLine(LogRow lr, String[] header) {
+
+        String[] s = new String[Array.getLength(header)];
+
+        s[0] = Double.toString(lr.getExcelDateTime());
+        s[1] = Integer.toString(lr.getDateTime().get(Calendar.HOUR_OF_DAY));
+        s[2] = Integer.toString(lr.getDateTime().get(Calendar.MINUTE));
+
+        for (int i = 3; i < Array.getLength(header); i++){
+            if (lr.getColumnLabel().get(i-3).equals(lr.getDescription())) {
+                s[i] = Integer.toString(lr.getCrates());
+            }
+            else {
+                s[i] = "0";
+            }
+        }
+
+        return s;
+    }
+
+    @Override
+    public List<String[]> toCSV(List<?> l) {
+
+        List<LogRow> lr = (List<LogRow>) l;
+        List<String[]> ls = new ArrayList<>();
+
+        String[] header = new String[3 + lr.get(0).getColumnLabel().size()];
+        header[0] = "DateTime";
+        header[1] = "Hour";
+        header[2] = "Minute";
+        for (int i = 3; i < Array.getLength(header); i++){
+            header[i] = lr.get(0).getColumnLabel().get(i-3);
+        }
+
+        System.out.println();
+
+        ls.add(header);
+        for (LogRow p : lr) {
+            ls.add(buildCsvLine(p, header));
+        }
+
+        return ls;
+    }
+
+
+}
---
+++ logic/src/com/cimcorp/plc/logParser/Main.java
@@ -0,0 +1,102 @@
+package com.cimcorp.plc.logParser;
+
+import com.custom.ArgNotFoundException;
+import com.custom.FileNotFoundException;
+import csvUtils.CSVUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static final String PATH = Paths.get(".").toAbsolutePath().normalize().toString() + "\\";
+    public static final String CONFIG_FILE = PATH + "config.ini";
+
+    public static void main(String[] args) throws FileNotFoundException, ArgNotFoundException {
+
+        if (Array.getLength(args) == 0){
+            throw new ArgNotFoundException("LogFileToParse");
+        }
+
+        final String logFileStr = PATH + args[0];
+
+        File configFile = new File(CONFIG_FILE);
+        File logFile = new File(logFileStr);
+
+        if (!configFile.exists()){
+            throw new FileNotFoundException(configFile);
+        }
+
+        if (!logFile.exists()){
+            throw new FileNotFoundException(logFile);
+        }
+
+        final String outputFile = PATH + "output.csv";
+
+        List<String[]> csvLines = null;
+        try {
+            csvLines = CSVUtil.read(logFileStr,",");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        final String[] header = csvLines.get(0);
+        csvLines.remove(0);
+
+        List<LogRow> logEntries = new ArrayList<>();
+        List<String[]> badLines = new ArrayList<>();
+        for (String[] s : csvLines) {
+            if (Array.getLength(s) >= 6) {
+                logEntries.add(new LogRow(s));
+            }
+            else{
+                badLines.add(s);
+            }
+        }
+        csvLines = null;
+
+        logEntries = logEntries.stream()
+                .filter(p->p.getHeader().contains("MET"))
+                .filter(p->p.getDateTime().get(Calendar.DAY_OF_MONTH)==9)
+                .collect(Collectors.toList());
+
+        List<String> descriptions = new ArrayList<>();
+        logEntries.stream()
+                .peek(p -> {
+                    if (!descriptions.contains(p.getDescription())) {
+                        descriptions.add(p.getDescription());
+                    }
+                })
+                .forEach(p -> {
+                    p.setColumnLabel(descriptions);
+                });
+
+
+        try {
+            CSVUtil.writeObject(logEntries,outputFile,",");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println();
+
+    }
+
+}
+
